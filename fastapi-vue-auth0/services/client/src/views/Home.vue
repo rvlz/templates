688ec334cf634ac2b43e9ca68da866bee44e6710
Ricardo Veloz <rick@rvlz.io>
2020-07-29 17:01:26 -0500

    add custom message to home view for logged in users

--- fastapi-vue-auth0/services/client/src/views/Home.vue
+++ fastapi-vue-auth0/services/client/src/views/Home.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>Home</div>
+  <div>
+    <h1>Home</h1>
+    <p v-if="!$auth.loading && $auth.isAuthenticated">Welcome, {{ currentUser }}</p>
+  </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    currentUser () {
+      return this.$store.state.profile.username
+    }
+  }
+}
+</script>
