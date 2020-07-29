83e3627f0a644c3e3ba03a887d5f2871a593915c
Ricardo Veloz <rick@rvlz.io>
2020-07-29 16:58:59 -0500

    move environment variables to configuration file

---
+++ fastapi-vue-auth0/services/client/src/config.js
@@ -0,0 +1,4 @@
+export const domain = process.env.VUE_APP_AUTH0_DOMAIN
+export const clientId = process.env.VUE_APP_AUTH0_CLIENT_ID
+export const audience = process.env.VUE_APP_AUTH0_AUDIENCE
+export const serviceAPIURL = process.env.VUE_APP_API_SERVICE_URL
